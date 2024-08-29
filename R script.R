66ddf74dc7db473ed12ed24a1fa0a5e5f912ec40
terry105040819 <91253450+terry105040819@users.noreply.github.com>
2024-08-29 15:04:31 +0800

    Update R script.R

--- R script.R
+++ R script.R
@@ -1,13 +1,43 @@
-control_rmoutlier <-  Control[Control$case_no %in% control_otu_rmoutlier$case_no,]
-control_rmoutlier_otu <- control_rmoutlier[,c(2,3,10,12)]
-control_rmoutlier_otu <- control_rmoutlier_otu %>% group_by(case_no,species) %>% summarize(hits=sum(num_hits))
-control_rmoutlier_table <- as.data.frame(pivot_wider(control_rmoutlier_otu, names_from = species, values_from = hits))
-control_rmoutlier_table<- lapply(control_rmoutlier_table, unlist)
-control_rmoutlier_table <- data.frame(lapply(control_rmoutlier_table, `length<-`, max(lengths(control_rmoutlier_table))))
-control_rmoutlier_table[is.na(control_rmoutlier_table)] <- 0 
+library(optparse)
+library(dplyr)
+library(tidyr)
+args = commandArgs(trailingOnly=TRUE)
 
-control_rmoutlier_otu <- as.data.frame(control_rmoutlier_otu)
-control_rmoutlier_otu <- control_rmoutlier_otu |>
-  dplyr::mutate(shannon_index = vegan::diversity(hits), .by = case_no)
-control_rmoutlier_otu <- control_rmoutlier_otu[!control_rmoutlier_otu$shannon_index %in% boxplot.stats(control_rmoutlier_otu$shannon_index)$out,]
-control_rmoutlier_otu$group <- "健康族群"
+option_list <- list(
+  make_option(c("-l","--taxonomy_level"),type="character",default = FALSE,help = "OTU table taxonomy resolution(ex.Species,Genus,Family)",metavar="character"),
+  make_option(c("-i","--input"),type ="character",default = FALSE,help = "taxonomy table directory",metavar="character"),
+  make_option(c("-o","--output"),action = "store",type = 'character',default = FALSE,help="OTU table output directory",metavar="character")
+)
+
+opt_parser <- OptionParser(option_list = option_list,
+                           add_help_option = TRUE,
+                           prog = NULL)
+
+opt <- parse_args(opt_parser)
+
+if (is.null(opt$input)){
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
+
+taxonomy_level <- opt$taxonomy_level
+input <- opt$input
+output <- opt$output
+
+
+taxo_table <- read.csv(file = input,header = T,sep = ",")
+  #col 10 species
+  #col 9 genus
+  #col 8 family
+  #Species_level
+otu <- taxo_table[,c(2,3,8,9,10,12)]
+if (taxonomy_level == "species") {
+  otu <- otu %>% group_by(case_no,species) %>% summarize(hits=sum(num_hits))
+  otu_table <- as.data.frame(pivot_wider(otu, names_from = species, values_from = hits))
+  otu_table <- lapply(otu_table, unlist)
+  otu_table <- data.frame(lapply(otu_table, `length<-`, max(lengths(otu_table))))
+  otu_table[is.na(otu_table)] <- 0 
+  write.csv(otu_table,file = paste(output,"csv",sep = "."),row.names = F)
+}else print("please type right Taxonomic rank with family,genus,species ")
+
+
